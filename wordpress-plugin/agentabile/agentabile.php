7bfe66ad616c15297ff8d06d37421efb1db86a23
Alex <villeneuve2527@icloud.com>
2026-07-23 17:03:09 +0200

    Differenzia Author URI da Plugin URI nell'header del plugin
    WordPress.org segnalava Plugin URI e Author URI identici
    (entrambi https://agentabile.dev). Author URI ora punta al profilo
    GitHub dell'autore (github.com/AlCap27), Plugin URI resta il sito
    del progetto.

--- wordpress-plugin/agentabile/agentabile.php
+++ wordpress-plugin/agentabile/agentabile.php
@@ -7,7 +7,7 @@
  * Requires at least: 6.0
  * Requires PHP:      7.4
  * Author:            Agentabile
- * Author URI:        https://agentabile.dev
+ * Author URI:        https://github.com/AlCap27
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       agentabile
